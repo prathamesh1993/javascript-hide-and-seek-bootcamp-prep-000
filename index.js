b7903c0fcf64acc411bdf020dac5558fb2f6dc89
PRATHAMESH PRABHUDESAI <prathamesh1993@gmail.com>
2018-10-08 01:51:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,5 +16,5 @@ function increaseRankBy(n){
 }
 
 function deepestChild(){
-  returndocument.getElementById('nested').querySelector('div.target')
+  return document.getElementById('grand-node').querySelector('div')
 }
