98ba46f851f8cc5846b754513ac1363fa56f54f2
PRATHAMESH PRABHUDESAI <prathamesh1993@gmail.com>
2018-10-08 01:52:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
 
+function nestedTarget(){
+  return document.getElementById('nested').querySelector('div.target');
+}
+
+function increaseRankBy(n){
+  const lis = document.querySelectorAll('ul.ranked-list li');
+  
+  for (let i = 0; i < lis.length; i++) {
+  lis[i].innerHTML = (parseInt(lis[i].innerHTML) + n).toString();
+    
+  }
+}
+
+function deepestChild(){
+  return document.getElementById('grand-node').querySelector('div').querySelector('div').querySelector('div').querySelector('div')
+}
